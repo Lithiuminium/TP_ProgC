f06e02a5b5888ce2aca7af5fb3e8420e6b6a91bc
Lithiuminium <145119264+Lithiuminium@users.noreply.github.com>
2025-04-16 14:59:47 +0200

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,59 @@
+#include <stdio.h>
 
+int main() {
+    int num1, num2, resultat;
+    char op;
+
+    printf("Entrez le premier nombre : ");
+    scanf("%d", &num1);
+
+    printf("Entrez le deuxième nombre : ");
+    scanf("%d", &num2);
+
+    printf("Entrez l'opérateur (+, -, *, /, %%, &, |, ~) : ");
+    scanf(" %c", &op);
+
+    switch (op) {
+        case '+':
+            resultat = num1 + num2;
+            break;
+        case '-':
+            resultat = num1 - num2;
+            break;
+        case '*':
+            resultat = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0)
+                resultat = num1 / num2;
+            else {
+                printf("Division par zéro\n");
+                return 1;
+            }
+            break;
+        case '%':
+            if (num2 != 0)
+                resultat = num1 % num2;
+            else {
+                printf("Modulo par zéro\n");
+                return 1;
+            }
+            break;
+        case '&':
+            resultat = num1 & num2;
+            break;
+        case '|':
+            resultat = num1 | num2;
+            break;
+        case '~':
+            resultat = ~num1;
+            break;
+        default:
+            printf("Opérateur invalide\n");
+            return 1;
+    }
+
+    printf("Résultat : %d\n", resultat);
+
+    return 0;
+}
