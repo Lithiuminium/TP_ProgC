d944d6d4746d64e63c0346829f5b3f256ae2284d
Lithiuminium <145119264+Lithiuminium@users.noreply.github.com>
2025-04-30 14:40:17 +0200

    Update puissance.c

--- TP2/src/puissance.c
+++ TP2/src/puissance.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int a = 2; 
-    int b = 3; 
+    int a = 2;
+    int b = 3;
     int resultat = 1;
-    if (b < 0) {
-        printf("L'exposant doit être un entier positif ou nul.\n");
-        return 1;
-    }
 
     for (int i = 0; i < b; i++) {
-        resultat *= a; 
+        resultat *= a;
     }
+
     printf("%d à la puissance %d est : %d\n", a, b, resultat);
 
     return 0;
